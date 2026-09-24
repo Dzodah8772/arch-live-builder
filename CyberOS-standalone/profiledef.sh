897161bb301d4840322d6789679fb20d18777c28
даниил <daniil2016haha@gmail.com>
2026-09-24 07:51:55 +0300

    CyberOS: switch live desktop to bspwm and finish Arch profile

--- CyberOS-standalone/profiledef.sh
+++ CyberOS-standalone/profiledef.sh
@@ -2,18 +2,19 @@
 iso_name="cyberos"
 iso_label="CYBEROS_$(date +%Y%m)"
 iso_publisher="CyberOS"
-iso_application="CyberOS Minimal"
+iso_application="CyberOS Minimal Gaming"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-buildmodes=('iso')
-bootmodes=('bios.syslinux' 'uefi.systemd-boot')
+buildmodes=("iso")
+bootmodes=("uefi-x64.systemd-boot")
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19')
+airootfs_image_type="squashfs"
 declare -A file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.bash_profile"]="0:0:644"
   ["/usr/local/bin/cyber-install"]="0:0:755"
+  ["/usr/local/bin/cyberos-nvidia-status"]="0:0:755"
   ["/etc/skel/.xinitrc"]="0:0:644"
   ["/etc/skel/.bashrc"]="0:0:644"
 )
